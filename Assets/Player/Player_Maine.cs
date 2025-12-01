f67e44a0bf1432b79650f518c3fcb4065ddab813
miyazaki <yuihito5468@outlook.jp>
2025-12-01 18:55:00 +0900

    階段の上りの速度と下りの速度の追加

--- Assets/Player/Player_Maine.cs
+++ Assets/Player/Player_Maine.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Player_Maine : MonoBehaviour
 {
+    //====== Playerの速度 ======
+    //下り速度
+    [Header("下りる速度")]
+    [Tooltip("階段を下りていく速度の変更")]
+    [SerializeField]
+    private float downhillSpeed = 0.01f;
+
+    //上り速度
+    [Header("上り速度")]
+    [Tooltip("階段を上る速度の変更")]
+    [SerializeField]
+    private float UpSpeed = 0.001f;
+
     // Start is called before the first frame update
     void Start()
     {
